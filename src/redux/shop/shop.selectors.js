821333bb9bc04cad11b0347cee8373d690c7bce6
rmonteroh <rmonteroh90@gmail.com>
2019-09-29 23:17:53 -0500

    Arreglando error en la pagina de shop

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,6 +7,12 @@ export const selectShopCollection = createSelector(
     shop => shop.collections
 )
 
+// Convirtiendo las colecciones en arreglos.
+export const selectCollectionsForPreview = createSelector(
+    [selectCollection],
+    shop => Object.keys(shop.collections).map(key => shop.collections[key])
+)
+
 export const selectCategory = categoryUrlParam => 
     createSelector(
         [selectCollection],
